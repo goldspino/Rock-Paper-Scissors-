3b3c2ba87c71542cf236f7082eca7f6b12070629
Sathish Ramachandran <sathish.r26@gmail.com>
2021-01-20 02:10:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+print("Hi! Today we are going to play rock, paper, scissors. The game is simple. 0 = rock, 1 = paper, and 2 = scissors. Let the game begin!")
+print("Now, type in 0, 1, or 2")
+input()
+RPS = ["rock", "paper", "scissors"]
+print (random.choice(RPS))
